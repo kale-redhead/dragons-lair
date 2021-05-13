09202ab80e9f9ba896549e49a97ff98a05aec7b7
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-05-13 08:20:15 -0400

    step 5 front and backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
 const authCtrl = require('./controllers/authController');
+const treasureCtrl = require('./controllers/treasureController');
 
 const app = express();
 const PORT = 4000;
@@ -30,5 +31,7 @@ app.use(
 app.post('/auth/register', authCtrl.register);
 app.post('/auth/login', authCtrl.login);
 app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
 
 app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
