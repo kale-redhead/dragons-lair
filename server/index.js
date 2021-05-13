e234311115716d7172268c00fde3a654c6e13c70
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-05-13 11:07:28 -0400

    step 6 backend and middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const massive = require('massive');
 const authCtrl = require('./controllers/authController');
 const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
 
 const app = express();
 const PORT = 4000;
@@ -33,5 +34,6 @@ app.post('/auth/login', authCtrl.login);
 app.get('/auth/logout', authCtrl.logout);
 
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
 
 app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
